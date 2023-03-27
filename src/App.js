9fd5e2aa0e16c75f387dca4f9ef201832e48485f
YoungBae-park <yb@bag-yeongbaeui-MacBookPro-2.local>
2023-03-27 13:03:08 +0900

    프로젝트 시작

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
 
-function App() {
+// 처음에는 현재날짜를 표시하는 기능을 만든다.
+// 날짜를 표현하는 텍스트 , 버튼을 클릭시 클릭시점의 날짜와 시간표시
+
+
+
+const App = () => {
+
+
+
+
+  const [standardTime, setStandardTime] = useState('');
+  
+  const today = new Date();
+  console.log(today);
+
+
+
+  const onReset = () => {
+     setStandardTime('소중이마지막물갈이시각 : '+today.toDateString()+"/"+today.toTimeString());
+     console.log('re : '+standardTime);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {standardTime}
+      <button onClick={onReset} >리셋</button>
     </div>
   );
 }
