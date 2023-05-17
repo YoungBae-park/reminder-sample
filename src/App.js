44e54d357880d5a659131f672d4796de4f7c84c8
YoungBae-park <yb@bag-yeongbaeui-MacBookPro-2.local>
2023-05-17 15:13:22 +0900

    hello3

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import React from 'react';
 //import 'semantic-ui-css/semantic.min.css'
 //import { Button, Form, Container, Header } from 'semantic-ui-react'
 //import file from './date.txt';
-//import axios from 'axios';
+import axios from 'axios';
 
 // 처음에는 현재날짜를 표시하는 기능을 만든다.
 // 날짜를 표현하는 텍스트 , 버튼을 클릭시 클릭시점의 날짜와 시간표시
@@ -28,7 +28,13 @@ const App = () => {
 //const data = new FormData();
 const url = "https://script.google.com/macros/s/AKfycbzPoYDfcSv2cDbQRc-A3tH3YmBA3OeDBhbvHOEzaAjPooRzS6N5GFzTdnopKoZpRqcD/exec"
 //data.append("title", "hello");
-const data = {'title': 'hello'}
+const data = {
+    id: 'AKfycbzPoYDfcSv2cDbQRc-A3tH3YmBA3OeDBhbvHOEzaAjPooRzS6N5GFzTdnopKoZpRqcD',
+    email: 'guriguriyang@gmail.com',
+    title: 'test',
+    body: {'hello':'world'},
+    status: 'type' === 'DELETE' ? "delete" : "reserve"
+  };
 let getResponse;
     const requestOptions = {
         method: 'POST',
@@ -40,7 +46,11 @@ let getResponse;
 
   async function getValue (){
     try {
-      let getResults = await fetch(url,requestOptions);
+      let getResults = await axios.post(url+ "?action=update",data, {
+            headers: {
+              "Content-Type": "text/plain;charset=utf-8"
+            }
+          });
       console.log("getResults");
       console.log(getResults.headers);
     getResponse = await getResults.json();
